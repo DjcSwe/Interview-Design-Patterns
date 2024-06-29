8981a86b5b04dcfcf98ff3a4a88c90a0cc7eb221
Daniel Condly <settle.never@aol.com>
2024-06-28 21:35:00 -0400

    Remove Nth Node

---
+++ two_pointers/remove_nth_node.py
@@ -0,0 +1,70 @@
+def remove_nth_last_node(head, n):
+    right = head
+    left = head
+
+    for i in range(n):
+        right = right.next
+
+    if not right:
+        return head.next
+
+    while right.next:
+        right = right.next
+        left = left.next
+
+    left.next = left.next.next
+
+    return head
+
+
+class LinkedList:
+    # __init__ will be used to make a LinkedList type object.
+    def __init__(self):
+        self.head = None
+
+    # insert_node_at_head method will insert a LinkedListNode at head
+    # of a linked list.
+    def insert_node_at_head(self, node):
+        if self.head:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
+
+    # create_linked_list method will create the linked list using the
+    # given integer array with the help of InsertAthead method.
+    def create_linked_list(self, lst):
+        for x in reversed(lst):
+            new_node = LinkedListNode(x)
+            self.insert_node_at_head(new_node)
+
+    # __str__(self) method will display the elements of linked list.
+    def __str__(self):
+        result = ""
+        temp = self.head
+        while temp:
+            result += str(temp.data)
+            temp = temp.next
+            if temp:
+                result += ", "
+        result += ""
+        return result
+
+    class LinkedListNode:
+        # __init__ will be used to make a LinkedListNode type object.
+        def __init__(self, data, next=None):
+            self.data = data
+            self.next = next
+
+
+def print_list_with_forward_arrow(linked_list_node):
+    temp = linked_list_node
+    while temp:
+        print(temp.data, end=" ")  # print node value
+
+        temp = temp.next
+        if temp:
+            print("→", end=" ")
+        else:
+            # if this is the last node, print null at the end
+            print("→ null", end=" ")
